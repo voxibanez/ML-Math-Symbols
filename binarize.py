198e8b19ef86eb1277f8245f9b6825d49217d983
Jacob Rothmel <jacob_rothmel@student.uml.edu>
2017-12-03 21:36:43 -0500

    Added in code to convert an image to true black and white.

---
+++ binarize.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+The this file contains code to convert a colored image to a pure
+black and white image. 
+"""
+
+from PIL import Image
+from scipy.misc import imsave
+import numpy
+
+def image_to_binary(src, dest, threshold):
+    """
+    The purpose of this method is to take the image at the src path
+    and save it as a black and white only image at the destination path.
+    
+    args:
+        -src (string): path to image to convert
+        -dest (string): path to save the image too
+        -threshold (integer): threshold value for pixels (where the white
+                                and black cutoff is)
+    
+    return:
+        -N/A
+    """
+    image_to_convert = Image.open(src)
+    
+    #convert the image to monochrome
+    monochrome_image = image_to_convert.convert('L')
+    
+    #get pixel array
+    monochrome_image = numpy.array(monochrome_image)
+    
+    #binarize array
+    binary_image_array = make_binary_array(monochrome_image, threshold)
+    
+    
+    
+def make_binary_array(convert_array, theshold=200)
+    """
+    The purpose of this function is to convert an array of pixel values
+    to a binary array based on the threshold.
+    
+    args:
+        -convert_array (numpy array): the array to convert
+        -theshold (integer) (default=200): the cuttoff value for pixels
+    
+    return:
+        -convert_array (numpy array): the converted binary array:
+    """
+    for i in range(len(convert_array)):
+        for j in range(len(convert_array[0])):
+            if convert_array[i][j] > threshold:
+                convert_array[i][j] = 255
+            else:
+                convert_array[i][j] = 0
+    return convert_array
+    
